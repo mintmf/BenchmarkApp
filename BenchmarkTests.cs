f1f52c08cc0a844f18b79adca8c26365b55337ba
mint_mf <froars@yandex.ru>
2024-10-03 01:57:36 +0300

    Добавьте файлы проекта.

---
+++ BenchmarkTests.cs
@@ -0,0 +1,59 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkApp
+{
+    public class BenchmarkTests
+    {
+        [Benchmark]
+        public List<int> BenchmarkTest1()
+        {
+            var intList = new List<int>();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                intList.Add(new Random().Next(0, 1000000));
+            }
+
+            return intList;
+        }
+
+        [Benchmark]
+        public List<int> BenchmarkTest2()
+        {
+            var intList = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                intList.Add(new Random().Next(0, 100));
+            }
+
+            return intList;
+        }
+
+        [Benchmark]
+        public List<int> BenchmarkTest3()
+        {
+            var intList = new List<int>();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                intList.Add(Convert.ToInt32(i.ToString()));
+            }
+
+            return intList;
+        }
+
+        [Benchmark]
+        public List<int> BenchmarkTest4()
+        {
+            var intList = new List<int>();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                intList.Add(i);
+            }
+
+            return intList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkApp;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Hello, World!");
+var result = BenchmarkRunner.Run<BenchmarkTests>();
+Console.ReadLine();
